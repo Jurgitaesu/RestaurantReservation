836cc6a75077f87fae7bf48d7fe2ef7df638814d
Jurgitaesu <60357936+Jurgitaesu@users.noreply.github.com>
2020-03-19 23:48:08 +0200

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,50 @@
+var form = document.querySelector("form");
+var maximumGuests = document.getElementById("maximumGuests");
+var availableReservations = document.getElementById("availableReservations");
+
+var restaurant = {
+    name: "Tasty",
+    maximumGuests: 30,
+    reserved: 0,
+    availableReservations: 30,
+
+    reserve: function (guests) {
+        if ((this.maximumGuests - this.reserved) >= guests) {
+            swal("You made a reservation for " + guests + " guest /-s");
+            this.reserved += guests;
+            this.availableReservations = this.maximumGuests - guests;
+            availableReservations.textContent = this.availableReservations;
+        } else {
+            swal("Please pick a lower number of guests.");
+        }
+    },
+
+    cancel: function (guests) {
+        if (this.reserved >= guests) {
+            this.reserved -= guests;
+            this.availableReservations = this.availableReservations + guests;
+            availableReservations.textContent = this.availableReservations;
+            swal("You canceled a reservation for " + guests + " guest /-s");
+        } else {
+            swal("Please pick a lower number of guests.");
+        }
+    }
+}
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    var guests = Number(form["input"].value);
+    var select = document.getElementById("select");
+    if (select.value == "reserve" && guests > 0) {
+        restaurant.reserve(guests);
+    } else if (select.value == "cancel" && guests > 0) {
+        restaurant.cancel(guests);
+    }
+    select.value = "";
+    input.value = "";
+
+})
+
+maximumGuests.textContent = restaurant.maximumGuests;
+
+availableReservations.textContent = restaurant.availableReservations;
